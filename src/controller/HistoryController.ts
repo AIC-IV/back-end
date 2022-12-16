f06dbe531334482349be37d81828b037573b1a6d
Arthur <arthur.souza@anota.ai>
2022-12-15 22:39:45 -0300

    match change

--- src/controller/HistoryController.ts
+++ src/controller/HistoryController.ts
@@ -3,10 +3,21 @@ import { Request, Response } from 'express';
 import HistoryService from '../services/History.Service';
 import MatchService from '../services/Match.Service';
 import UserService from '../services/User.Service';
-
 export async function createHistory(req: Request, res: Response) {
 
-  const {results , matchId} = req.body;
+  const {results } = req.body;
+
+  function makeid(length: number) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+  const fullmatch: any = MatchService.createMatch(makeid(25));
 
   results.forEach(async (result: any) => {
     let { userId, points, placement } = result;
@@ -14,7 +25,7 @@ export async function createHistory(req: Request, res: Response) {
       return res.status(400).json({ message: 'User not found' });
     }
 
-    const match = await MatchService.getMatchByID(matchId);
+    const match = await MatchService.getMatchByID(fullmatch.id);
     if (match === null) {
       return res.status(400).json({ message: 'Match not found' });
     }
@@ -29,7 +40,7 @@ export async function createHistory(req: Request, res: Response) {
   }
 
   try {
-    const history = await HistoryService.createHistory(userId, matchId, points, placement);
+    const history = await HistoryService.createHistory(userId, fullmatch.id, points, placement);
     await UserService.addPoints(userId, points);
     return res.json(history).status(200);
   } catch (error) {
